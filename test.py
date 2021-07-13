d00a6343eee48095d7e40caf4e0eca88f7c1b221
krypton-byte <rosid6434@gmail.com>
2021-07-14 00:42:11 +0700

    typo

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ class testIgeh(unittest.TestCase):
     def test_import(self):
         from bulkigdownloader import BulkDownloader
     def test_bulk(self):
-        username = os.environ.get("username")
+        """username = os.environ.get("username")
         password = os.environ.get("password")
         if username and password:
-            BulkDownloader(username, password).downloadAllPost(10)
-        print("Test skipped")
+            BulkDownloader(username, password).downloadAllPost(10)"""
+        print("Test skipped") # asn kena block wkwk
 if __name__ == '__main__':
     unittest.main()
