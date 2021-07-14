d84285c51ffc1c8dd9252d30b3ab6a83f9f8f359
krypton-byte <rosid6434@gmail.com>
2021-07-14 13:24:40 +0700

    hadeh

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ class testIgeh(unittest.TestCase):
     def test_import(self):
         from bulkigdownloader import BulkDownloader
     def test_bulk(self):
-        BulkDownloader(cookie_path="sessions/instagram.com_cookies.txt").downloadAllPost(10)
+        """BulkDownloader(cookie_path="sessions/instagram.com_cookies.txt").downloadAllPost(10)"""
+        #gk jadi hampir kena ban
+        pass
 
 if __name__ == '__main__':
     unittest.main()
