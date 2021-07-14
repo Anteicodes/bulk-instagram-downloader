f85a0ffaee785eb2ac9491998aede4cf089e6fa2
krypton-byte <rosid6434@gmail.com>
2021-07-14 18:41:05 +0700

    fix max

--- bulkigdownloader/__main__.py
+++ bulkigdownloader/__main__.py
@@ -11,14 +11,20 @@ argument.add_argument("--max", help="Maximum take media from posts, default 20")
 argument.add_argument("--type", help="Bulk type e.g: post, ")
 argument.add_argument("--worker", help="default 3")
 parse = argument.parse_args()
-
+if parse.worker:
+    if parse.worker.isnumeric():
+        worker = int(parse.worker)
+    else:
+        worker = 3
+else:
+    worker = 3
 if parse.type == 'post':
     if parse.token:
         bulk=BulkDownloader(cookie_path=parse.token)
-        bulk.downloadAllPost( parse.worker if parse.worker.isnumeric() else 3 if parse.worker else 3)
+        bulk.downloadAllPost(parse.max if parse.max else 20,worker)
     elif parse.username and parse.password:
         bulk = BulkDownloader(parse.username, parse.password)
-        bulk.downloadAllPost(parse.max if parse.max else 20, parse.worker if parse.worker.isnumeric() else 3 if parse.worker else 3)
+        bulk.downloadAllPost(parse.max if parse.max else 20, worker)
     else:
         os.system(f"python3 -m {dirname(__file__).split('/')[-1]} --help")
 else:
--- bulkigdownloader/post_bulk.py
+++ bulkigdownloader/post_bulk.py
@@ -36,7 +36,7 @@ class BulkDownloader:
 
     def getAllPost(self, max)->dict:
         for user in self.get_all_following:
-            all_post = self.instagram.get_medias_by_user_id(user.identifier, int(max) if max.isnumeric() else user.media_count)
+            all_post = self.instagram.get_medias_by_user_id(user.identifier, int(max) if type(max) == str and max.isnumeric() else user.media_count)
             for index, i in enumerate(all_post, 1):
                 stdout.write(f"\rScrapping from {user.username} => {index}/{len(all_post)} post {round((index/all_post.__len__())*100)}%            ")
                 res=igdownload(i.link if i.link[-1] == "/" else i.link+"/", self.instagram.generate_headers(self.instagram.user_session))
