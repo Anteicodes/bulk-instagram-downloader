504fd2b7590e1f718b30378b4db4160523071611
krypton-byte <rosid6434@gmail.com>
2021-07-14 19:39:07 +0700

    fix instatools

--- bulkigdownloader/post_bulk.py
+++ bulkigdownloader/post_bulk.py
@@ -40,6 +40,10 @@ class BulkDownloader:
             for index, i in enumerate(all_post, 1):
                 stdout.write(f"\rScrapping from {user.username} => {index}/{len(all_post)} post {round((index/all_post.__len__())*100)}%            ")
                 res=igdownload(i.link if i.link[-1] == "/" else i.link+"/", self.instagram.generate_headers(self.instagram.user_session))
+                if not res["status"]:
+                    res = igdownload(i.link, self.instagram.generate_headers(self.instagram.user_session))
+                    if not res["status"]:
+                        res["result"] = []
                 res.update({"created_at":i.created_time})
                 stdout.flush()
                 yield {user.username:res}
